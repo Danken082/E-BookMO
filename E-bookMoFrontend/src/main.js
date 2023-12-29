c65739e8a0e1fcc566425bd0806b616b5265bf8e
Nicolle <130277462+nicsxxgutierrez@users.noreply.github.com>
2023-12-29 11:58:51 +0800

    main

--- E-bookMoFrontend/src/main.js
+++ E-bookMoFrontend/src/main.js
@@ -4,6 +4,6 @@ import './registerServiceWorker'
 import router from './router'
 import axios from 'axios'
 
-axios.defaults.baseURL="http://localhost:8080/"
+axios.defaults.baseURL="https://roomrental.online/"
 createApp(App).use(router).mount('#app')
     
